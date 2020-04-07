e3ddd69baee6b0e8ce3a3157d4690790b8d1a6cd
Robet Master <61965948+master-dxt@users.noreply.github.com>
2020-04-07 10:15:26 +0700

    TampilanTermux

---
+++ TampilanTermux.py
@@ -0,0 +1,23 @@
+#!/bin/sh
+#color
+
+pkg update -y
+clear
+pkg upgrade -y
+clear
+pkg install cl -y
+clear
+pkg install vim -y
+clear 
+pkg install ruby -y
+clear
+gem install lolcat
+clear
+pkg install cowsay
+clear 
+pkg install toilet -y
+clear
+pkg install neofetch -y
+clear
+pkg install nano
+echo "Pleas accept........"
