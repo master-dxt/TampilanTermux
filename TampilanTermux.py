1724cffcb5c9e544b91e1b4e7b447e370df477c0
Robet Master <61965948+master-dxt@users.noreply.github.com>
2020-04-07 10:34:27 +0700

    TampilanTermux

--- TampilanTermux.py
+++ TampilanTermux.py
@@ -20,4 +20,27 @@ clear
 pkg install neofetch -y
 clear
 pkg install nano
-echo "Pleas accept........"
+#!/bin/sh
+#color
+
+pkg update -y
+clear
+pkg upgrade -y
+clear
+pkg install cl -y
+clear
+pkg install vim -y
+clear 
+pkg install ruby -y
+clear
+gem install lolcat -y
+clear
+pkg install cowsay -y
+clear 
+pkg install toilet -y
+clear
+pkg install neofetch -y
+clear
+pkg install nano -y
+cd ../usr/etc
+echo "masukan perintah...bash.bashrc"
