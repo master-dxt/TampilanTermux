e755763cfc7eadb30c2df80a360c0fb9c9f49fa0
Robet Master <61965948+master-dxt@users.noreply.github.com>
2020-04-08 07:16:39 +0700

    TampilanTermux

--- TampilanTermux.py
+++ TampilanTermux.py
@@ -2,45 +2,16 @@
 #color
 
 pkg update -y
-clear
 pkg upgrade -y
-clear
 pkg install cl -y
-clear
 pkg install vim -y
-clear 
 pkg install ruby -y
-clear
-gem install lolcat
-clear
-pkg install cowsay
-clear 
-pkg install toilet -y
-clear
-pkg install neofetch -y
-clear
-pkg install nano
-#!/bin/sh
-#color
-
-pkg update -y
-clear
-pkg upgrade -y
-clear
-pkg install cl -y
-clear
-pkg install vim -y
-clear 
-pkg install ruby -y
-clear
 gem install lolcat -y
-clear
 pkg install cowsay -y
-clear 
 pkg install toilet -y
-clear
 pkg install neofetch -y
-clear
 pkg install nano -y
 cd ../usr/etc
-echo "masukan perintah... nano bash.bashrc "
+
+echo "masukan perintah........."
+"nano bash.bashrc"
